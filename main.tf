39af78f8567d25b168db640a5f550fe8c59a680f
Yuvaraj <grootmac@Yuvarajs-MacBook-Air.local>
2025-10-31 23:16:46 +0530

    added ci

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "yuvaraj" {
   ami           = "ami-0c02fb55956c7d316" # Replace with a valid AMI ID for your region
   instance_type = "t2.micro"             # Choose your desired instance type
   tags = {
-    Name = "Yuvaraj"
+    Name = "Groot"
   }
 }
 
