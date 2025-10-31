aaf8d9e0df817bf53f17d7e48d148f2641bd0e27
Yuvaraj <grootmac@Yuvarajs-MacBook-Air.local>
2025-11-01 00:33:07 +0530

    added ci

---
+++ backend.tf
@@ -0,0 +1,8 @@
+# In your Terraform configuration file (e.g., main.tf), define the remote backend.
+terraform {
+  backend "s3" {
+    bucket         = "aws-terraform-test-12345 "
+    key            = "path/to/your/terraform.tfstate"
+    region         = "us-east-1" # Change to your desired region
+  }
+}
