c95c8bf8dbe86c1522ce64a5da62f3a4df06e17d
Yuvaraj <grootmac@Yuvarajs-MacBook-Air.local>
2025-10-31 21:45:57 +0530

    feat: added terraform code

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-0abcdef1234567890" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"             # Choose your desired instance type
+  tags = {
+    Name = "MyTerraformEC2"
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = "us-east-1" # Replace with your desired AWS region
+  access_key = "AKIAZWYFYUZLD2GGVPEB" # Not recommended for production
+  secret_key = "Yz+oqKE8j/495KqKfSViG+pSAIZ715D0pWZYCluEC" # Not recommended for production
+}
